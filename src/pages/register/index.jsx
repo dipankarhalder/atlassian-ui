69be8653ab8d4ce0c6785de43ba46f4de98b3c57
dipankarhalder <halderdip.dev@gmail.com>
2023-08-26 03:41:31 +0530

    added new pages and components with dynamic content

---
+++ src/pages/register/index.jsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+export function Register() {
+  return (
+    <div>
+      <h1>Register</h1>
+      <Link to="/">login</Link>
+    </div>
+  );
+}
---
+++ src/utils/content.js
@@ -0,0 +1,7 @@
+export const content = {
+  login: {
+    log_placeholder: 'Enter your email',
+    btn_content: 'Continue',
+    register_link: 'Register',
+  },
+};
